0cb1b5e4cdada2793a0b208e9c6b01cc28bc41de
Maniko <65548350+M4NIK0@users.noreply.github.com>
2022-07-24 10:42:18 +0200

    Added update file for 0.9.4

---
+++ Update.py
@@ -0,0 +1,23 @@
+import os, shutil, time
+
+MCMMPath = os.getcwd()
+
+versionFile = open(MCMMPath + '\\Version', 'r')
+updateFromVersion = versionFile.read().replace('\n', '')
+versionFile.close()
+
+versionFile = open(MCMMPath + '\\WorkDir\\Update\\Version', 'r')
+updateToVersion = versionFile.read().replace('\n', '')
+versionFile.close()
+
+print('Updating from ' + updateFromVersion + 'nothing to apply there')
+print('install update at', MCMMPath)
+shutil.copyfile(MCMMPath + '\\WorkDir\\Update\\MC_MM.py', MCMMPath + '\\MC_MM.py', follow_symlinks=True)
+print('MC_MM.py updated')
+shutil.copyfile(MCMMPath + '\\WorkDir\\Update\\MC_MM.py', MCMMPath + '\\MC_MM.py', follow_symlinks=True)
+print('Version file updated')
+shutil.copyfile(MCMMPath + '\\WorkDir\\Update\\lang\\en-us.lang', MCMMPath + '\\lang\\en-us.lang', follow_symlinks=True)
+print('en-us lang file updated')
+print('Update finished !\nStarting MCMM...')
+
+os.system('Run.bat')
